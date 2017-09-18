b51fde028fd75bb245970ca31aeec5ecc6558e14
Chakhsu Lau <zexu.liu@live.cn>
2017-09-18 15:28:29 +0800

    fix dir missing

--- core/index.js
+++ core/index.js
@@ -31,19 +31,24 @@ module.exports = (option) => {
 
 	const loadComponent = (app, obj, componentPath, flag = false) => {
 		return new Promise((resolve, reject) => {
-			fs.readdir(componentPath, function (err, files) {
-				if (err)
-					reject(err);
-				if(files) {
-					Promise.all(files.map((file) => {
-						const f = path.join(componentPath, file);
-						const name = path.posix.basename(file).split('.', 1)[0];
-						if (flag)
-							obj[name] = new(require(f)(app))();
-						else
-							obj[name] = require(f)(app);
-						debug(`require ${f}, done`);
-					})).then(resolve).catch(reject);
+			fs.access(componentPath, (err) => {
+				if(!err) {
+					fs.readdir(componentPath, function (err, files) {
+						if (err)
+							reject(err);
+						if(files) {
+							Promise.all(files.map((file) => {
+								const f = path.join(componentPath, file);
+								const name = path.posix.basename(file).split('.', 1)[0];
+								if (flag)
+									obj[name] = new(require(f)(app))();
+								else
+									obj[name] = require(f)(app);
+								debug(`require ${f}, done`);
+							})).then(resolve).catch(reject);
+						}
+					});
+
 				}
 			});
 		});
